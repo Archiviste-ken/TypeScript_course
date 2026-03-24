b8d7df1a5a07ae80c3c508a030360a84ef11c00a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-24 21:14:39 +0530

    Add greet function and username logging to index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+function greet(name: string): string {
+    return `Hello, ${name}, Welcome to the world of TypeScript!`;
+}
+
+const username: string =  "John Doe";
+
+console.log(greet(username));
+
+
+
+
+
+
